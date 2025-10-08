4f7e9988e5a9a712fa9066adb514a6c42b342f26
Jean Decian <jeandecian@hotmail.fr>
2025-10-07 22:44:15 -0400

    add approved products getter

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
+from bs4 import BeautifulSoup
 import requests
 
 WIKI_IOT_BASE_URL = "https://fehmijaafar.net/wiki-iot/index.php"
+APPROVED_REVS_TITLE = "Special:ApprovedRevs"
 
 
 def get_page(url=""):
     return requests.get(WIKI_IOT_BASE_URL + url).text
 
 
-print(get_page())
+def get_approved_products(limit=500, offset=0):
+    page = get_page(
+        f"?title={APPROVED_REVS_TITLE}&limit={limit}&offset={offset}&show=all"
+    )
+
+    soup = BeautifulSoup(page, "html.parser")
+    products = []
+    for tag in soup.find("ol", class_="special").find_all("li"):
+        text = tag.text
+
+        if "Template:" in text or "User:" in text:
+            continue
+
+        if text in ("Allowed Domains", "Calculator version", "Methodology"):
+            continue
+
+        text = text.split(" (revision")[0]
+        products.append(text)
+
+    if 'class="mw-nextlink"' in page:
+        return products + get_approved_products(limit, offset + limit)
+
+    return products
+
+
+print(get_approved_products())
